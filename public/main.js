b214d5c6ea19827c98d95a65e55fb143bd655688
Elena <57794968+3le81@users.noreply.github.com>
2024-03-18 23:04:55 +0000

    initialized autocompletion for UK places

--- public/main.js
+++ public/main.js
@@ -1,4 +1,4 @@
-let map, directionsService, directionsRenderer;
+let directionsService, directionsRenderer;
 
 function initMap() {
     directionsService = new google.maps.DirectionsService();
@@ -8,6 +8,20 @@ function initMap() {
         center: { lat: 51.509865, lng: -0.118092 }, // Example: London
     });
     directionsRenderer.setMap(map);
+
+    // Initialize autocomplete for origin input field - restricted to the UK!
+    const originInput = document.getElementById("from");
+    const originAutocomplete = new google.maps.places.Autocomplete(originInput, {
+        types: ['geocode'], // Specify the type of input (geocode for addresses)
+        componentRestrictions: { country: "uk" }
+    });
+
+    // Initialize autocomplete for destination input field - restricted to the UK!
+    const destinationInput = document.getElementById("to");
+    const destinationAutocomplete = new google.maps.places.Autocomplete(destinationInput, {
+        types: ['geocode'],
+        componentRestrictions: { country: "uk" }
+    });
 }
 
 async function calcRoute() {
@@ -38,8 +52,8 @@ async function loadGoogleMapsAPI() {
         const data = await response.json();
         const apiKey = data.apiKey;
 
-        var script = document.createElement('script');
-        script.src = `https://maps.googleapis.com/maps/api/js?key=${apiKey}&callback=initMap`;
+        const script = document.createElement('script');
+        script.src = `https://maps.googleapis.com/maps/api/js?key=${apiKey}&libraries=places,directions&callback=initMap`;
         script.async = true;
         script.defer = true;
         document.head.appendChild(script);
@@ -49,3 +63,59 @@ async function loadGoogleMapsAPI() {
 }
 
 loadGoogleMapsAPI();
+
+
+
+// OLD ORIGINAL CODE //
+
+// let map, directionsService, directionsRenderer;
+
+// function initMap() {
+//     directionsService = new google.maps.DirectionsService();
+//     directionsRenderer = new google.maps.DirectionsRenderer();
+//     map = new google.maps.Map(document.getElementById("googleMap"), {
+//         zoom: 7,
+//         center: { lat: 51.509865, lng: -0.118092 }, // Example: London
+//     });
+//     directionsRenderer.setMap(map);
+// }
+
+// async function calcRoute() {
+//     const origin = document.getElementById("from").value;
+//     const destination = document.getElementById("to").value;
+//     const output = document.querySelector('#output');
+
+//     const request = {
+//         origin: origin,
+//         destination: destination,
+//         travelMode: 'DRIVING'
+//     };
+
+//     directionsService.route(request, function (result, status) {
+//         if (status === 'OK') {
+//             directionsRenderer.setDirections(result);
+//             const route = result.routes[0].legs[0];
+//             output.innerHTML = `<div class='alert-info'>From: ${origin}.<br />To: ${destination}.<br />Driving distance <i class='fas fa-road'></i> : ${route.distance.text}.<br />Duration <i class='fas fa-hourglass-start'></i> : ${route.duration.text}.</div>`;
+//         } else {
+//             output.innerHTML = "<div class='alert-danger'><i class='fas fa-exclamation-triangle'></i> Could not display the map route.</div>";
+//         }
+//     });
+// }
+
+// async function loadGoogleMapsAPI() {
+//     try {
+//         const response = await fetch('http://localhost:3000/api/maps-api-key');
+//         const data = await response.json();
+//         const apiKey = data.apiKey;
+
+//         var script = document.createElement('script');
+//         script.src = `https://maps.googleapis.com/maps/api/js?key=${apiKey}&callback=initMap`;
+//         script.async = true;
+//         script.defer = true;
+//         document.head.appendChild(script);
+//     } catch (error) {
+//         console.error("Failed to load the Google Maps API key.", error);
+//     }
+// }
+
+// loadGoogleMapsAPI();
