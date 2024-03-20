01698e96dcef6d0b2357245c787349ef4e3a5eb6
Elena <57794968+3le81@users.noreply.github.com>
2024-03-20 11:57:17 +0000

    toggle menu functionality and mediaquery added

--- public/main.js
+++ public/main.js
@@ -1,3 +1,17 @@
+// Function to handle the menu toggle
+document.addEventListener("DOMContentLoaded", function () {
+    const menuToggle = document.querySelector(".menu-toggle");
+    const nav = document.querySelector("nav");
+
+    menuToggle.addEventListener("click", function () {
+        nav.classList.toggle("collapsed");
+        menuToggle.classList.toggle("active");
+    });
+});
+
+
+// google maps functions 
+
 let map, directionsService, directionsRenderer;
 
 function initMap() {
