c292eae71414ec29e0e780948471264574c72263
Jack L <jackktlam@gmail.com>
2024-03-20 17:56:44 +0000

    router mounting - mounted router pathing into server.js

--- server.js
+++ server.js
@@ -3,14 +3,16 @@ require('dotenv').config();
 const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
-const app = express();
 const path = require('path');
+const apiRouter = require('./src/routes/api');
+
+const app = express();
 const port = 3000;
 
 app.use(cors());
 app.use(express.json());
-
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/api', apiRouter);
 
 app.get('/api/maps-api-key', (req, res) => {
     res.json({ apiKey: process.env.GOOGLE_MAPS_API_KEY });
