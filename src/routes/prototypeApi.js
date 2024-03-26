6d5eb139d315cf1d19c8d2d0c07be7c9a0dd28bd
Jack L <jackktlam@gmail.com>
2024-03-26 13:02:12 +0000

    depreceated commit

--- src/routes/prototypeApi.js
+++ src/routes/prototypeApi.js
@@ -2,8 +2,26 @@ const express = require('express');
 const router = express.Router();
 const needle = require('needle');
 
-router.get('/', (req, res) => {
+const baseURL = process.env.prototype_base_url;
+const apiKey = process.env.prototype_apikey;
+
+// const prototypeApiFetch = require('../controllers/prototypeApiFetch');
+
+router.get('/', async (req, res) => {
     res.json({ success: true});
+    try {
+        const apiRes = await needle('get', `${baseURL}`);
+        const data = apiRes.body;
+
+        res.status(200).json(data);
+    } catch (error) {
+        res.send(500).json({error});
+    }
+
 })
+
+// router.get('/fetch', prototypeApiFetch)
+
+
 
 module.exports = router
